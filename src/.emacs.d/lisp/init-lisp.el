5c0c2b3a56a39aff60dc22d1c385a13c68313bcf
Chris Maier <chris@chris-maier.com>
2015-12-18 07:17:15 +0100

    lisp autocompletion and shortcut for function description

---
+++ src/.emacs.d/lisp/init-lisp.el
@@ -0,0 +1,16 @@
+;; Lisp autocompletion 
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
+;; S-k gets you lisp documentation 
+(require-package 'elisp-slime-nav)
+(defun my-lisp-hook ()
+  (elisp-slime-nav-mode)
+  (turn-on-eldoc-mode)
+  )
+(add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
+
+;; 'K' show elisp description in lisp mode
+(evil-define-key 'normal emacs-lisp-mode-map (kbd "K") 'elisp-slime-nav-describe-elisp-thing-at-point)
+
+(provide 'init-lisp)
