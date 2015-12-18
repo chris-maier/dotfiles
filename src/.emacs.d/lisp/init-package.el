99f97a6e6ac3495aff01796848563e5fb5293a01
Chris Maier <chris@chris-maier.com>
2015-12-18 07:14:00 +0100

    package configuratino from Aaron Bieber

---
+++ src/.emacs.d/lisp/init-package.el
@@ -0,0 +1,48 @@
+;;
+;; Package configuration provided by Aaron Bieber
+;; see: https://github.com/aaronbieber/dotfiles
+
+(require 'package)
+
+;;; Standard package repositories
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;;; On-demand installation of packages
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install package `%s': %S" package err)
+     nil)))
+
+;;; Fire up package.el
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(require-package 'fullframe)
+(fullframe list-packages quit-window)
+
+(require-package 'cl-lib)
+(require 'cl-lib)
+
+(provide 'init-package)
