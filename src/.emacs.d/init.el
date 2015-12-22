5acb185c3bc58bf6e7df125d8428b982b1161dcd
Chris Maier <chris@chris-maier.com>
2015-12-22 23:38:12 +0100

    store history

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -44,4 +44,14 @@
 ; active the powerline 
 (my-powerline-default-theme)
 
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
 (evil-mode t)
