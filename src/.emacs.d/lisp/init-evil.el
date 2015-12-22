849502ffdb8f898759148dd9a7609919b08fba39
Chris Maier <chris@chris-maier.com>
2015-12-22 23:13:52 +0100

    evil-nerd-commmenter added for better handling comments

--- src/.emacs.d/lisp/init-evil.el
+++ src/.emacs.d/lisp/init-evil.el
@@ -1,5 +1,9 @@
 ;; EVIL leader config 
+(require-package 'evil)
 (require-package 'evil-leader)
+(require-package 'evil-nerd-commenter)
+(require-package 'key-chord)
+
 (global-evil-leader-mode)
 
 (evil-leader/set-leader ",")
@@ -8,22 +12,20 @@
   "," 'other-window
   "o" 'delete-other-windows
   "w" 'save-buffer
-  "k" 'kill-buffer)
+  "k" 'kill-buffer
 
-;; package-install evil
-(require-package 'evil)
+  ;; nerd-commenter
+  "ci" 'evilnc-comment-or-uncomment-lines
+)
 
-;; package-install key-chord
-(require-package 'key-chord)
-(key-chord-mode t)
 
 (defun move-line-up ()
-;; Move line up one line  
+"Move line up one line"
   (interactive)
   (transpose-lines 1)
   (forward-line -2))
 (defun move-line-down ()
-;; Move line down one line
+"Move line down one line"
   (interactive)
   (forward-line 1)
   (transpose-lines 1)
@@ -36,6 +38,7 @@
 (define-key evil-normal-state-map (kbd "C-l") 'forward-word)
 (define-key evil-normal-state-map (kbd "C-j") '(lambda () (interactive) (next-line 5)))
 (define-key evil-normal-state-map (kbd "C-k") '(lambda () (interactive) (previous-line 5)))
+
 ; TODO - improvement store/restore cursor position
 (define-key evil-normal-state-map (kbd "C-S-j") 'move-line-down)
 (define-key evil-normal-state-map (kbd "C-S-k") 'move-line-up)
