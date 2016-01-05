21d0fa80ce8f39033cac27ac9b99485b6b88780e
Chris Maier <chris@chris-maier.com>
2016-01-05 23:41:16 +0100

    keep 'my/' namespace

--- src/.emacs.d/lisp/init-lisp.el
+++ src/.emacs.d/lisp/init-lisp.el
@@ -1,14 +1,14 @@
-;; Lisp autocompletion 
+;; Lisp autocompletion
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
-;; S-k gets you lisp documentation 
+;; S-k gets you lisp documentation
 (require-package 'elisp-slime-nav)
-(defun my-lisp-hook ()
+(defun my/lisp-hook ()
   (elisp-slime-nav-mode)
   (turn-on-eldoc-mode)
   )
-(add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
+(add-hook 'emacs-lisp-mode-hook 'my/lisp-hook)
 
 ;; 'K' show elisp description in lisp mode
 (evil-define-key 'normal emacs-lisp-mode-map (kbd "K") 'elisp-slime-nav-describe-elisp-thing-at-point)
