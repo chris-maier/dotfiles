5c17baf43703cb87aded9644feaa75ad3b57fe55
Chris Maier <chris@chris-maier.com>
2016-01-05 23:10:57 +0100

    keep the namespace 'my/' consistent

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -43,13 +43,13 @@
 (setq make-backup-files nil)
 (setq-default highlight-symbol-idle-delay 1.5)
 
-(defun my-font-set (myfont)
+(defun my/font-set (myfont)
   ;; My font settings
   (interactive)
   (when (member myfont (font-family-list))
     (set-face-attribute 'default nil :font myfont))
 )
-(my-font-set "DejaVu Sans Mono")
+(my/font-set "DejaVu Sans Mono")
 
 ;; activate the powerline
 (my-powerline-default-theme)
