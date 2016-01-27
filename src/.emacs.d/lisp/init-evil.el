176d557aefb0a58bae6f4fccdc2d25b7d8441d2d
Chris Maier <chris@chris-maier.com>
2016-01-27 10:52:45 +0100

    Org mode mappings added, line mode functions moved to init-utils.el, evil-jumper introduced

--- src/.emacs.d/lisp/init-evil.el
+++ src/.emacs.d/lisp/init-evil.el
@@ -1,17 +1,20 @@
 ;; EVIL leader config
 (require-package 'evil)
+(require-package 'evil-jumper)
 (require-package 'evil-leader)
 (require-package 'evil-nerd-commenter)
 (require-package 'key-chord)
+
+(require 'init-utils)
 
 (global-evil-leader-mode)
 
 (evil-leader/set-leader ",")
 (evil-leader/set-key
   ;; TODO - switch to helm
-  ;; "e" 'helm-find-file
+  "e" 'helm-find-files
   "," 'other-window
-  ;; "." 'switch-to-prev-buffer
+  "." 'switch-to-prev-buffer
   "o" 'delete-other-windows
   "w" 'save-buffer
   "k" 'kill-buffer
@@ -26,36 +29,52 @@
   "cc" 'evilnc-comment-or-uncomment-lines
 )
 
-;; We probably should move these functions to a seperate file
-(defun move-line-up ()
-"Move line up one line"
-  (interactive)
-  (transpose-lines 1)
-  (forward-line -2))
-
-(defun move-line-down ()
-"Move line down one line"
-  (interactive)
-  (forward-line 1)
-  (transpose-lines 1)
-  (forward-line -1))
-
 ;; Moving around
 (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
 (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
 (define-key evil-normal-state-map (kbd "C-h") 'evil-backward-word-begin)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-forward-word-begin)
-(define-key evil-normal-state-map (kbd "C-j") '(lambda () (interactive) (next-line 5)))
-(define-key evil-normal-state-map (kbd "C-k") '(lambda () (interactive) (previous-line 5)))
+(define-key evil-normal-state-map (kbd "C-j") '(lambda () (interactive) (ignore-errors (evil-next-visual-line 5))))
+(define-key evil-normal-state-map (kbd "C-k") '(lambda () (interactive) (ignore-errors (evil-previous-visual-line 5))))
 
+(define-key evil-normal-state-map (kbd "C-S-j") 'my/move-line-down)
+(define-key evil-normal-state-map (kbd "C-S-k") 'my/move-line-up)
+
+(define-key evil-normal-state-map (kbd "SPC") 'evil-search-forward)
+
+;; Move in VISUAL
+(define-key evil-visual-state-map (kbd "C-j") 'my/evil-next-visual-line)
+(define-key evil-visual-state-map (kbd "C-k") 'my/evil-previous-visual-line)
+(define-key evil-visual-state-map (kbd "C-S-j") 'my/move-region-down)
+(define-key evil-visual-state-map (kbd "C-S-k") 'my/move-region-up)
+
+;; PROJECTILE
 (define-key evil-normal-state-map (kbd "C-t") 'my/find-file)
 (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile)
 (define-key evil-normal-state-map (kbd "C-S-P") 'helm-projectile-switch-project)
 
-; TODO - improvement store/restore cursor position
-(define-key evil-normal-state-map (kbd "C-S-j") 'move-line-down)
-(define-key evil-normal-state-map (kbd "C-S-k") 'move-line-up)
-(define-key evil-normal-state-map (kbd "SPC") 'evil-search-forward)
+;; ORG-Mode
+(evil-define-key 'normal org-mode-map (kbd "C-S-l") 'org-shiftright)
+(evil-define-key 'normal org-mode-map (kbd "C-S-h") 'org-shiftleft)
+(evil-define-key 'insert org-mode-map (kbd "C-S-l") 'org-shiftright)
+(evil-define-key 'insert org-mode-map (kbd "C-S-h") 'org-shiftleft)
+;; Move org element right
+(evil-define-key 'normal org-mode-map (kbd "M-l") 'org-metaright)
+(evil-define-key 'insert org-mode-map (kbd "M-l") 'org-metaright)
+;; Move org element left
+(evil-define-key 'normal org-mode-map (kbd "M-h") 'org-metaleft)
+(evil-define-key 'insert org-mode-map (kbd "M-h") 'org-metaleft)
+;; Move org element up
+(evil-define-key 'normal org-mode-map (kbd "M-k") 'org-metaup)
+(evil-define-key 'insert org-mode-map (kbd "M-k") 'org-metaup)
+;; Move org element down
+(evil-define-key 'normal org-mode-map (kbd "M-j") 'org-metadown)
+(evil-define-key 'insert org-mode-map (kbd "M-j") 'org-metadown)
+
+;; Jumper Mode mappings
+(define-key evil-normal-state-map (kbd "C-+") 'evil-jump-to-tag)
+(define-key evil-normal-state-map (kbd "C-o") 'evil-jumper/backward)
+(define-key evil-normal-state-map (kbd "C-i") 'evil-jumper/forward)
 
 ;; 'jk' fast <ESC>
 (setq key-chord-two-keys-delay 0.2)
