480013cbc7273a22b70a6c61eb74c6cf3a8fd82b
Chris Maier <chris@chris-maier.com>
2016-05-13 08:55:20 +0200

    fixed: wrong number of arguments

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -7,6 +7,13 @@
 
 ;;; Code:
 ;; add "lisp" to the load-path
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; try to improve slow performance on windows.
@@ -26,7 +33,7 @@
 (setq visible-bell t)
 
 ;; Enable visual wrap lines
-(setq visual-line-mode)
+(setq visual-line-mode t)
 
 ;; Scrolling
 (setq scroll-margin 10)
