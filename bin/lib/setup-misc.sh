728ea5c188021c68a5b2808d250fa0a64064806a
Chris Maier <chris@chris-maier.com>
2017-07-29 18:42:54 +0200

    added fasd

--- bin/lib/setup-misc.sh
+++ bin/lib/setup-misc.sh
@@ -5,10 +5,12 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source shellib.sh
 CheckSudo
 
-packages="revelation truecrypt virtualbox virtualbox_qt"
+packages="revelation truecrypt virtualbox fasd"
 
 # add truecrypt repository
 add-apt-repository -y ppa:stefansundin/truecrypt
+# install fasd
+add-apt-repository ppa:aacebedo/fasd
 
 wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | apt-key add -
 echo "deb http://download.virtualbox.org/virtualbox/debian yakkety non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list
@@ -16,7 +18,7 @@ echo "deb http://download.virtualbox.org/virtualbox/debian yakkety non-free cont
 AptInstallList $packages
 
 # execute 3rd party printer script
-${script_dir}/linux-brprinter-installer-2.1.1-1 MFC-L2700DW
+# ${script_dir}/linux-brprinter-installer-2.1.1-1 MFC-L2700DW
 
 # link the keyboard layout file
 sudo -u $SUDO_USER ln -fs $(readlink -f $script_dir/../../src/.Xmodmap) ~/.Xmodmap
@@ -35,3 +37,4 @@ elif [ -n $XFCE4 ]; then
 else
 	EchoErr "No terminal shortcut set"
 fi
+
