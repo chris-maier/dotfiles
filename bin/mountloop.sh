aa283905890c1ab09cf74a2ec4f5c96b60da1ec2
Chris Langhans <chris@chris-maier.com>
2018-04-05 09:05:27 +0200

    Mount a file system image by partition number

--- bin/mountloop.sh
+++ bin/mountloop.sh
@@ -2,13 +2,18 @@
 # set -x
 
 # $1 image to mount
-# $2 mount point
+# $2 partition number
+# $3 mount point
 
-partfile=$1
-mountpoint=$2
+image=$1
+part=$2
+mountpoint=$3
+
+# constants
+sectorsize=512
 
 # Debug log
-verbose=""
+verbose="verbose"
 
 function EchoErr () {
     printf "%s\n" "$*" >&2
@@ -40,12 +45,22 @@ function CmdAvailable () {
     return $retval
 }
 
+# $1: number of arguments
+# $2: number of expected arguments
+function ParseArgs () {
+	if [ "$1" -ne "$2" ]; then
+		EchoErr "Illegal number of arguments."
+		exit 1
+	fi
+}
+
 # __Main__
 CheckSudo
 CmdAvailable "partx"
+ParseArgs $# 3
 
 # check if image exits
-if [ ! -f "$partfile" ] ; then
+if [ ! -f "$image" ] ; then
     EchoErr "Could not find image."
     exit 1
 fi
@@ -53,11 +68,28 @@ fi
 # check mount point???
 # if [ -z ]
 
-partmap=$(partx -bg $partfile)
+partmap=$(partx -bg $image -n $part)
 if [ "${partmap:=x}" == "x" ]; then
     EchoErr "partx returned null string."
+	exit 1
+else
+	# truncate offset and partition size from partition table
+	offset=$(awk '{print $2}' <<< $partmap)
+	size=$(awk '{print $5}' <<< $partmap)
+
+	offset=$(expr $offset \* $sectorsize)
 fi
 
-while IFS= read -r line ; do
-    echo $line;
-done <<< "${partmap}"
+mkdir -p $mountpoint
+mount -o loop,offset=$offset,sizelimit=$size -o defaults $image $mountpoint
+## output partition table line by line
+# while IFS= read -r line ; do
+#     echo $line;
+# done <<< "${partmap}"
+
+## User interaction
+# mount part number 1 on default
+# mntnum=1
+# read -p "Mount partition number [$mntnum]: " input
+# mntnum="${input:-$mntnum}"
+# EchoDebug $mntnum
